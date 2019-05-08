41b4b15a7636395ed2527c4b9e402c23845b0a2b
MaxIndu <maxindupro@gmail.com>
2019-05-08 08:48:44 +0530

    Added display components

--- src/app/inventory/product/product.component.ts
+++ src/app/inventory/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  public product:Product;
+
   constructor() { }
 
   ngOnInit() {
+    this.product = new Product('T-Shirt', 12.55, 'https://cdn.shopify.com/s/files/1/0668/2853/products/LittleMan-Tshirt-2_1.jpg?v=1531522189', true);
+  }
+
+  addToCart(event){
+    console.log('Adding to cart');
+
   }
 
 }
